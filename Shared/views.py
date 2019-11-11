142092c3d434463ab4bf642f92f643e3d8332dad
Abhinav Sharma <abhinavsharma629@gmail.com>
2019-11-12 00:25:17 +0530

    notes saving process update

--- Shared/views.py
+++ Shared/views.py
@@ -133,8 +133,6 @@ def getFriends(request):
         print(sharedNoteData.objects.filter(noteId=savedNoteData.objects.get(noteId=request.GET.get('noteId'))).values('sharedTo'))
         print(UserFriends.objects.get(userId=UserDetails.objects.get(userId=request.user)).friends.all())
         print(UserFriends.objects.get(userId=UserDetails.objects.get(userId=request.user)))
-        for(i in UserFriends.objects.get(userId=UserDetails.objects.get(userId=request.user)).friends.all()):
-            print(i)
         sharedUsers=UserFriends.objects.get(userId=UserDetails.objects.get(userId=request.user)).friends.all().exclude(friend_name_id__in=sharedNoteData.objects.filter(noteId=savedNoteData.objects.get(noteId=request.GET.get('noteId'))).values('sharedTo'))
 
         print(sharedUsers)
