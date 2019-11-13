7088b1c385a696379c170637829d019fc2682c7a
Abhinav Sharma <abhinavsharma629@gmail.com>
2019-11-13 06:13:24 +0530

    signup update

--- Users/views.py
+++ Users/views.py
@@ -212,15 +212,16 @@ def createUser(request, format=None):
 def saveImage(request, format=None):
     parser=(MultiPartParser,)
     params=request.data
+    print(request.FILES)
     print(params)
     print(params['_parts'][0][1]['uri'])
 
-    obj,notif=User.objects.get_or_create(username="aaashdkahsdhasdha", email="abhinavsharma622@gmail.com")
+    obj,notif=User.objects.get_or_create(username="aaaashdkahsdhasdha", email="abhinavsharma622@gmail.com")
     obj.set_password("abhi629@@")
     if(notif):
         obj.save()
         date = parse_date("2019-08-09")
-        obj1,notif1=UserDetails.objects.get_or_create(userId=User.objects.get(username="aaashdkahsdhasdha"), address="sddsfsfsdfs", address1="asdadsadsad", phoneNumber="7909948987", occupation="sdffffffff", state="ads", city="ads", country="asd", alternatePhoneNumber="9557806467", dateOfBirth=date, gender="M")
+        obj1,notif1=UserDetails.objects.get_or_create(userId=User.objects.get(username="aaaashdkahsdhasdha"), address="sddsfsfsdfs", address1="asdadsadsad", phoneNumber="7909948987", occupation="sdffffffff", state="ads", city="ads", country="asd", alternatePhoneNumber="9557806467", dateOfBirth=date, gender="M")
         if(notif1):
             obj1.save()
             obj1.lat_long='POINT('+str("23.56")+' '+str("34.67")+')'
