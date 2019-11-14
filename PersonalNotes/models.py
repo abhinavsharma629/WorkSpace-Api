00c6b51c40cd6b36f987b64dc82db99cb0c7b363
Abhinav Sharma <abhinavsharma629@gmail.com>
2019-11-14 17:53:41 +0530

    notes pictures update

--- PersonalNotes/models.py
+++ PersonalNotes/models.py
@@ -7,16 +7,17 @@ class savedNoteData(models.Model):
     noteId=models.AutoField(primary_key=True)
     userId= models.ForeignKey(UserDetails, on_delete=models.CASCADE)
     noteData=models.TextField()
-    
+
     #-------------------------------------------------
     # Seperate This Later To Another Model
     gitHubData=JSONField(null=True, blank=True)
     typeOfData=models.CharField(max_length=100, null=True, blank=True)
     #-------------------------------------------------
-    
+
     title=models.CharField(max_length=1000)
     caption=models.TextField(null=True, blank=True)
     createdFrom=models.CharField(max_length=100, null=True, blank=True)
     showUpImg=models.TextField(null=True, blank=True)
+    showUpImg1=models.FileField(upload_to='notes_pictures', null=True, blank=True)
     createdAt=models.DateTimeField(auto_now=True)
     lastUpdated=models.DateTimeField()
