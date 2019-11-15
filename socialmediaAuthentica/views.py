4d0807525f2ffe44169a23d4ebbd0719b806eb5e
Abhinav Sharma <abhinavsharma629@gmail.com>
2019-11-16 02:47:06 +0530

    gd_updates

--- socialmediaAuthentica/views.py
+++ socialmediaAuthentica/views.py
@@ -265,7 +265,8 @@ flow1 = OAuth2WebServerFlow(client_id='484263106620-gqflub2lb8d0bvbof404133q236u
                             'https://www.googleapis.com/auth/drive.file',
                             'https://www.googleapis.com/auth/drive.appdata'
                             ],
-                            
+                            access_type='offline',
+                            prompt='consent',
                             redirect_uri='https://obscure-bayou-10492.herokuapp.com/test/complete/google-oauth2/')
 
 
