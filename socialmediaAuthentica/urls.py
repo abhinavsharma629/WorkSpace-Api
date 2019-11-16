355fcc195d1a745b00e035db460d5019814baf1a
Abhinav Sharma <abhinavsharma629@gmail.com>
2019-11-16 05:49:22 +0530

    gd_updates

--- socialmediaAuthentica/urls.py
+++ socialmediaAuthentica/urls.py
@@ -11,11 +11,9 @@ urlpatterns = [
     #Google Auth -D
     path('complete/google-oauth2/', views.complete, name="complete"),
     path('login', views.login, name="login"),
-    path('gd_login_auth_uri', views.gd_login_auth_uri, name="gd_login_auth_uri"),
-    path('gd_oauth2_for_native/', views.gd_oauth2_for_native, name="gd_oauth2_for_native"),
     path('gd_segregates', views.gd_segregates, name="gd_segregates"),
     path('gd_selected_segregates', views.gd_selected_segregates, name="gd_selected_segregates"),
-    
+
     #GitHub Auth -D
     path('complete/gitHub-oauth2', views.gitHubComplete, name="gitHubComplete"),
     path('gitHubLogin', views.gitHubLogin, name="gitHubLogin"),
