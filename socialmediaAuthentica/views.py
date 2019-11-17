f46035eb18926f2766d9251d14be1cc4a77b01d9
Abhinav Sharma <abhinavsharma629@gmail.com>
2019-11-18 04:02:38 +0530

    gd_updates

--- socialmediaAuthentica/views.py
+++ socialmediaAuthentica/views.py
@@ -520,6 +520,7 @@ def buildDriveForDrive(request):
 def rootFolderDataForDrive(request):
     if(DataAnalysis.objects.filter(user=request.user, classificationOfDataStorageType="ROOT FOLDER DATA").count()>0):
         print(type(DataAnalysis.objects.get(user=request.user, classificationOfDataStorageType="ROOT FOLDER DATA").rootPageData))
+        print(DataAnalysis.objects.get(user=request.user, classificationOfDataStorageType="ROOT FOLDER DATA").rootPageData[0:9])
         return JsonResponse({'message':'success', 'status':200})
     else:
         return JsonResponse({'message':'Drive Not Built', 'status':'404'})
